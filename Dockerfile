c88c3a4ea617cb2a113f57a59f7615047a225d2c
ktomkow <krzysztof.tomkow@gmail.com>
2022-03-28 20:50:41 +0200

    Work on docker compose and appsettings

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /app
 COPY . ./
 
-RUN dotnet publish OnlineShop.sln -c Release -o out
+RUN dotnet publish OnlineShop.sln -c Debug -o out
 
 FROM mcr.microsoft.com/dotnet/aspnet:6.0
 
 COPY --from=build /app/out /app
 WORKDIR /app
 
-EXPOSE 5222
+EXPOSE 8000
 CMD ["dotnet","WebApi.dll"]
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", true, true);
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
