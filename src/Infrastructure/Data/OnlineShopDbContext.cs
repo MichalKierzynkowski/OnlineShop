5199537d2306b39a9f4176911af4fb58433dd55b
michu3136 <71141129+michu3136@users.noreply.github.com>
2022-03-29 18:31:50 +0200

    Add DbContext Class

---
+++ src/Infrastructure/Data/OnlineShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class OnlineShopDbContext:DbContext
+    {
+        public OnlineShopDbContext()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
