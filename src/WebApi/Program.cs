39ca35b3e21c7966ae87f35565a714ca2cd5834c
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-03-30 12:47:47 +0200

    Add SwagerEndPoint

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -7,13 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
 }
 
 app.UseAuthorization();
