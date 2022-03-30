622a957c0fff294289960a788c5db1f934061f51
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-03-30 14:55:46 +0200

    Add Dependency Injection of Services.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,10 @@
+using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,15 +13,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddSingleton(AutoMaperConfig.Initialize());
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShopOnline Api", Version = "v1.0" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1.0"));
 }
 
 app.UseAuthorization();
