6da3e574e651da69d0b68176ef17d884291104fa
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-03-30 11:40:06 +0200

    Add DbSet<> with all entities.

--- src/Infrastructure/Data/OnlineShopDbContext.cs
+++ src/Infrastructure/Data/OnlineShopDbContext.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
     public class OnlineShopDbContext:DbContext
     {
-        public OnlineShopDbContext()
+        public OnlineShopDbContext(DbContextOptions options):base(options)
         {
             
         }
-
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CustomerDetail> CustomerDetails { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProductDetail> ProductDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+           
         }
     }
 }
