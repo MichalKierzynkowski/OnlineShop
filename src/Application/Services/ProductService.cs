f46d5fd3ea8504d67fa0fabb99265c3e05336a22
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-03-30 14:25:16 +0200

    Add AutoMapper

--- src/Application/Services/ProductService.cs
+++ src/Application/Services/ProductService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,38 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
 
+        }
+        public Product AddNewCategory(Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
