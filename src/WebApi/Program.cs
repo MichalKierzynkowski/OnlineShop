08dbcd34e4f1f2ffa6693df6cb2dbc766cca811c
ktomkow <krzysztof.tomkow@gmail.com>
2022-03-30 21:47:09 +0200

    Use in memory database, register by service provider from infrastructure project

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,8 +1,6 @@
 using Application.Interfaces;
-using Application.Mappings;
 using Application.Services;
-using Domain.Interfaces;
-using Infrastructure.Repositories;
+using Infrastructure;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,14 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddSingleton(AutoMaperConfig.Initialize());
+
+builder.Services.RegisterTestInfrastructure();
+
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddSwaggerGen(c =>
 {
     c.EnableAnnotations();
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShopOnline Api", Version = "v1.0" });
 });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
