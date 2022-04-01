7647b3c734d872738a21bc5a1be55cac15c3aeef
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-04-01 10:32:05 +0200

    Add Implementation of GetAll, GetById, Add method in CategoryRepository

--- src/Infrastructure/Repositories/CategoryRepository.cs
+++ src/Infrastructure/Repositories/CategoryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,37 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly OnlineShopDbContext _context;
+        public CategoryRepository(OnlineShopDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Category> GetAll()
+        {
+           return _context.Categories;
+        }
 
+        public Category GetById(Guid id)
+        {
+            return _context.Categories.SingleOrDefault(p => p.Id == id);
+        }
+        public Category Add(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return category;
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
