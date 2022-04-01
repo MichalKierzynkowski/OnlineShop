568292a236eff4f45ce80a57de2b85a679bbfa1b
ktomkow <krzysztof.tomkow@gmail.com>
2022-04-02 00:07:26 +0200

    Introduce base api controller

--- src/WebApi/Controllers/CategoryController.cs
+++ src/WebApi/Controllers/CategoryController.cs
@@ -2,49 +2,40 @@
 using Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoryController:ControllerBase
+  public class CategoryController : BaseApiController
+  {
+    private readonly ICategoryService _categoryService;
+    public CategoryController(ICategoryService categoryService)
     {
-        private readonly ICategoryService _categoryService;
-        public CategoryController(ICategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
-
-
-        [HttpGet]
-        [SwaggerOperation(Summary = "Retrieves all categories")]
-        public IActionResult GetAll()
-        {
-            var categories = _categoryService.GetAllCategories();
-            return Ok(categories);
-        }
-        [HttpGet("{id}")]
-        [SwaggerOperation(Summary = "Retrieves one category")]
-
-        public IActionResult GetById(Guid id)
-        {
-            var category = _categoryService.GetCategoryById(id);
-            return Ok(category);
-        }
-
-        [HttpPost]
-        [SwaggerOperation(Summary = "Create new Category")]
-        public IActionResult Create(CreateCategoryDto newCategory)
-        {
-            var id = _categoryService.AddNewCategory(newCategory);
-            return Created($"api/category/{id}", newCategory);
-
-        }
-
+      _categoryService = categoryService;
     }
+
+    [HttpGet]
+    [SwaggerOperation(Summary = "Retrieves all categories")]
+    public IActionResult GetAll()
+    {
+      var categories = _categoryService.GetAllCategories();
+      return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    [SwaggerOperation(Summary = "Retrieves one category")]
+
+    public IActionResult GetById(Guid id)
+    {
+      var category = _categoryService.GetCategoryById(id);
+      return Ok(category);
+    }
+
+    [HttpPost]
+    [SwaggerOperation(Summary = "Create new Category")]
+    public IActionResult Create(CreateCategoryDto newCategory)
+    {
+      var id = _categoryService.AddNewCategory(newCategory);
+      return Created($"api/category/{id}", newCategory);
+    }
+  }
 }
