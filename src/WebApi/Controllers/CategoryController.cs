804efecfcc0f1743ea51e1e5aef81f408825a734
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-04-01 10:57:21 +0200

    Add GetAll, GetById, CreateNewCategory in CategoryController

--- src/WebApi/Controllers/CategoryController.cs
+++ src/WebApi/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Dto.Category;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,37 @@ namespace WebApi.Controllers
     [ApiController]
     public class CategoryController:ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Retrieves all categories")]
+        public IActionResult GetAll()
+        {
+            var categories = _categoryService.GetAllCategories();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        [SwaggerOperation(Summary = "Retrieves one category")]
+
+        public IActionResult GetById(Guid id)
+        {
+            var category = _categoryService.GetCategoryById(id);
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Create new Category")]
+        public IActionResult Create(CreateCategoryDto newCategory)
+        {
+            var id = _categoryService.AddNewCategory(newCategory);
+            return Created($"api/category/{id}", newCategory);
+
+        }
 
     }
 }
