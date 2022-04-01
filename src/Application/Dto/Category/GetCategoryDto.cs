5c55b22db60e2a42b9fed0a9282c3f78889c825d
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-04-01 12:13:08 +0200

    Repair problem with ID

--- src/Application/Dto/Category/GetCategoryDto.cs
+++ src/Application/Dto/Category/GetCategoryDto.cs
@@ -8,6 +8,7 @@ namespace Application.Dto.Category
 {
     public  class GetCategoryDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
     }
