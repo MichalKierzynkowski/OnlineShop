d3ded718c8fc265a02de16ea2c8df9f2c0f75dc3
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-04-01 10:26:57 +0200

    Add Implementation to GetAllCategories and GetCategoryById to CategoryServices.

--- src/Application/Services/CategoryServices.cs
+++ src/Application/Services/CategoryServices.cs
@@ -6,16 +6,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Interfaces;
 
 namespace Application.Services
 {
     public class CategoryServices:ICategoryServices
     {
-        public CategoryServices()
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+        public CategoryServices(ICategoryRepository repository, IMapper mapper)
         {
-
+            _repository = repository;
+            _mapper = mapper;
         }
 
+        public IEnumerable<GetCategoryDto> GetAllCategories()
+        {
+            var categories = _repository.GetAll();
+            return _mapper.Map<IEnumerable<GetCategoryDto>>(categories);
+        }
+
+        public GetCategoryDto GetCategoryById(Guid id)
+        {
+            var category = _repository.GetById(id);
+            return _mapper.Map<GetCategoryDto>(category);
+        }
         public Guid AddNewCategory(CreateCategoryDto newCategory)
         {
             throw new NotImplementedException();
@@ -26,15 +42,7 @@ namespace Application.Services
             throw new NotImplementedException();
         }
 
-        public IEnumerable<GetCategoryDto> GetAllCategories()
-        {
-            throw new NotImplementedException();
-        }
-
-        public GetCategoryDto GetCategoryById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public void UpdateCategory(Guid id, Category category)
         {
