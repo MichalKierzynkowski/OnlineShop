5b854155b6b95d9b8aca5706514cce80ab653466
ktomkow <krzysztof.tomkow@gmail.com>
2022-04-23 16:04:55 +0200

    Use serilog in web app

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,29 @@
+using Serilog;
 using WebApi;
 
-Host.CreateDefaultBuilder()
-    .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
-    .Build()
-    .Run();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting service");
+    Host.CreateDefaultBuilder()
+        .UseSerilog((context, services, configuration) => configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .WriteTo.Console())
+        .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+        .Build()
+        .Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "An unhandled exception occured during bootstrapping");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
