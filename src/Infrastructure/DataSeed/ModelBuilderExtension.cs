3864ea3fe07bee8db5e0ba52b566bcde9f436a9b
Michal Kierzynkowski <michal.kierzynkowski@gmail.com>
2022-04-27 15:06:38 +0200

    Add DataSeed method to OnModelCreating. Add static method to ModeBuilderExtension class

--- src/Infrastructure/DataSeed/ModelBuilderExtension.cs
+++ src/Infrastructure/DataSeed/ModelBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.DataSeed
 {
-    internal class ModelBuilderExtension
+    public static class ModelBuilderExtensions
     {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
